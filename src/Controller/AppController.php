76865eb8f7e574ab9c848951ed17c6550273fece
COil <vernet.loic@gmail.com>
2021-01-09 22:41:22 +0100

    bootstrap app

---
+++ src/Controller/AppController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    #[Route('/', name: 'app')]
+    public function index(): Response
+    {
+        return $this->render('app/homepage.html.twig');
+    }
+}
